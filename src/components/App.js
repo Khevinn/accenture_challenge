c32abd19be6fcd1746535604ca98e71d59d029ea
Khevin Karlos <Karimoxiha@gmail.com>
2020-10-03 18:37:48 -0300

    Frontend

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+<img src="https://s2.glbimg.com/3WBpcofhxcpzqBrPZoUUeEfXhc0=/0x0:695x401/600x0/smart/filters:gifv():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/9/4/j6fITqTW2nce3A8ZA3IQ/2-nokia-3310.jpg" usemap="#image-map"/>
+<textarea name="return" id="" cols="15" rows="6"></textarea>
+<map name="image-map">
+
+    <area target="" alt="tela" title="tela" href="" coords="250,96,351,165" shape="rect"/>
+    <area target="" alt="enter" title="enter" href="" coords="326,193,276,181" shape="rect"/>
+    <area target="" alt="numero2" title="numero2" href="" coords="288,232,314,244" shape="rect"/>
+    <area target="" alt="numero3" title="numero3" href="" coords="327,224,354,240" shape="rect"/>
+    <area target="" alt="numero4" title="numero4" href="" coords="273,263,248,249" shape="rect"/>
+    <area target="" alt="numero5" title="numero5" href="" coords="285,255,317,268" shape="rect"/>
+    <area target="" alt="numero6" title="numero6" href="" coords="327,248,351,263" shape="rect"/>
+    <area target="" alt="numero7" title="numero7" href="" coords="249,270,276,288" shape="rect"/>
+    <area target="" alt="numero8" title="numero8" href="" coords="288,276,315,290" shape="rect"/>
+    <area target="" alt="numero9" title="numero9" href="" coords="328,269,349,286,425,313" shape="rect"/>
+    <area target="" alt="numero0" title="numero0" href="" coords="313,315,289,302" shape="rect"/>
+
+</map>
+        </div>
+        );
+    }
+
+}
